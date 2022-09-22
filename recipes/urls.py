b093134cb2878ce9c143b62c61c00cbb9b94bfb2
mlknub <mlknubcrypto@gmail.com>
2022-09-22 14:23:56 -0300

    urls

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from recipes.views import contatos, home, sobre
+
+urlpatterns = [
+
+    path('', home),
+    path('sobre/', sobre),
+    path('contatos/', contatos),
+]
