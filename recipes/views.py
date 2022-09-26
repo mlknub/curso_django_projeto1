dd388191591a7c2bbf42161e00f841957c1d96a2
mlknub <mlknubcrypto@gmail.com>
2022-09-26 10:25:47 -0300

    Criando a área de busca (search)

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Batata', })
+
+    return render(request, 'recipes/pages/home.html')
